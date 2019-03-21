72f3c41eef729039290f673674179caaa3c82100
HalieStamm9 <48777551+HalieStamm9@users.noreply.github.com>
2019-03-21 02:58:06 +0100

    Create u.js

---
+++ extension/u.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    if (window.location.href.indexOf('//www.google') == -1) {
+        return
+    }
+    $('head').append('<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">')
+    s = $('#search div.g').last()
+
+    $('div i').last().find('a').each(function (i, a) {
+        if (a.textContent != 'Complaint') {
+            return
+        }
+        $.ajax({
+            type: 'GET',
+            url: 'https://cors-anywhere.herokuapp.com/' + a.href,
+            dataType: 'html',
+            success: function (data) {
+                links = data.match(/>(https?:\/\/[^<]+)<\/li>/g)
+                for (i in links) {
+                    u = links[i].replace('</li>', '').replace('>', '')
+                    s.after('<div class="g">'
+                        + '<a href="' + u + '" target="_blank">' + u + '</a>'
+                        + '</div>'
+                    )
+                }
+            },
+            error: function (e, err) {
+                console.log(err);
+            }
+        });
+    })
+
+
+});
+
