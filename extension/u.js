1ec2d961e2059d3a87dc6b27a868bfa6842c5ccb
ibit <48777551+Ibit-to@users.noreply.github.com>
2019-04-06 01:12:30 +0200

    Switch CORS anywhere domain to fix https://github.com/Ibit-to/google-unlocked/issues/9

--- extension/u.js
+++ extension/u.js
@@ -2,24 +2,24 @@ $(function () {
     if (window.location.href.indexOf('//www.google') === -1) {
         return
     }
-    var s = $('#search div.g').last()
+    s = $('#search div.g').last()
 
     $('div i > a').each(function (i, a) {
         if (a.href.toLowerCase().indexOf('chillingeffects') === -1) {
             return
         }
-        var id = a.href.replace(/https?:\/\/www\.chillingeffects\.org\/notice.cgi\?sID=/, '')
-        var url = 'https://www.lumendatabase.org/notices/' + id
+        id = a.href.replace(/https?:\/\/www\.chillingeffects\.org\/notice.cgi\?sID=/, '')
+        url = 'https://www.lumendatabase.org/notices/' + id
         if (navigator.userAgent.toLowerCase().indexOf('firefox') === -1) {
-            url = 'https://cors-anywhere.herokuapp.com/' + url
+            url = 'https://secret-ocean-49799.herokuapp.com/' + url
         }
         $.ajax({
             type: 'GET',
             url: url,
             dataType: 'html',
             success: function (data) {
-                var links = data.match(/>(https?:\/\/[^<]+)<\/li>/g)
-                for (var i in links) {
+                links = data.match(/>(https?:\/\/[^<]+)<\/li>/g)
+                for (i in links) {
                     u = links[i].replace('</li>', '').replace('>', '')
                     s.after('<div class="g">'
                         + '<a href="' + u + '" target="_blank">' + u + '</a>'
